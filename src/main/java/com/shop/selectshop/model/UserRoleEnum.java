e75408531aa77652445fb1610090576fb0eeddec
sehwan <goodya1f@gmail.com>
2022-05-05 21:34:38 +0900

    Feat: 회원가입기능 추가

---
+++ src/main/java/com/shop/selectshop/model/UserRoleEnum.java
@@ -0,0 +1,6 @@
+package com.shop.selectshop.model;
+
+public enum UserRoleEnum {
+    USER, // 사용자 권한
+    ADMIN // 관리자 권한
+}
---
+++ src/main/java/com/shop/selectshop/service/UserService.java
@@ -0,0 +1,45 @@
+package com.shop.selectshop.service;
+
+import com.shop.selectshop.dto.SignupRequestDto;
+import com.shop.selectshop.model.User;
+import com.shop.selectshop.model.UserRoleEnum;
+import com.shop.selectshop.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC"; // 무의미한 랜담한값 임의로 삽입
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void registerUser(SignupRequestDto requestDto) {
+        String username = requestDto.getUsername();
+        String password = requestDto.getPassword();
+// 회원 ID 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
+        }
+
+        String email = requestDto.getEmail();
+// 사용자 ROLE 확인
+        UserRoleEnum role = UserRoleEnum.USER;
+        if (requestDto.isAdmin()) {
+            if (!requestDto.getAdminToken().equals(ADMIN_TOKEN)) {
+                throw new IllegalArgumentException("관리자 암호가 틀려 등록이 불가능합니다.");
+            }
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, password, email, role);
+        userRepository.save(user);
+    }
+}
