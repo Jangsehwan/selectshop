df970aee7bdc293e4993f94c23c83224dd444ba2
sehwan <goodya1f@gmail.com>
2022-05-22 23:08:31 +0900

    Refactor: Validator 패키지로 분리(new validator package(ProductValidator, URLValidator))

--- src/main/java/com/shop/selectshop/model/Product.java
+++ src/main/java/com/shop/selectshop/model/Product.java
@@ -1,6 +1,7 @@
 package com.shop.selectshop.model;
 
 import com.shop.selectshop.dto.ProductRequestDto;
+import com.shop.selectshop.validator.ProductValidator;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -43,9 +44,9 @@ public class Product {
     // 관심 상품 생성 시 이용합니다.
     public Product(ProductRequestDto requestDto, Long userId) {
         // 입력값 Validation
-        validateProductInput(requestDto, userId);
+        ProductValidator.validateProductInput(requestDto, userId);
 
-// 관심상품을 등록한 회원 Id 저장
+        // 관심상품을 등록한 회원 Id 저장
         this.userId = userId;
         this.title = requestDto.getTitle();
         this.image = requestDto.getImage();
@@ -54,39 +55,4 @@ public class Product {
         this.myprice = 0;
     }
 
-    private void validateProductInput(ProductRequestDto requestDto, Long userId) {
-        if (userId == null || userId <= 0) {
-            throw new IllegalArgumentException("회원 Id 가 유효하지 않습니다.");
-        }
-
-        if (requestDto.getTitle() == null || requestDto.getTitle().isEmpty()) {
-            throw new IllegalArgumentException("저장할 수 있는 상품명이 없습니다.");
-        }
-
-        if (!isValidUrl(requestDto.getImage())) {
-            throw new IllegalArgumentException("상품 이미지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (!isValidUrl(requestDto.getLink())) {
-            throw new IllegalArgumentException("상품 최저가 페이지 URL 포맷이 맞지 않습니다.");
-        }
-
-        if (requestDto.getLprice() <= 0) {
-            throw new IllegalArgumentException("상품 최저가가 0 이하입니다.");
-        }
-    }
-
-    boolean isValidUrl(String url)
-    {
-        try {
-            new URL(url).toURI();
-            return true;
-        }
-        catch (URISyntaxException exception) {
-            return false;
-        }
-        catch (MalformedURLException exception) {
-            return false;
-        }
-    }
 }
